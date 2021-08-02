5b7af98ad3ce88c4fb682263202d0ce9f9b904c0
Mikey <michaelp.esteban@gmail.com>
2021-08-02 11:34:29 -0400

    added all photo urls from Unsplash

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,31 +7,27 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-photo = Unsplash::Photo.find("tAKXap853rY")
+intro_photo = Unsplash::Photo.find('XtUd5SiX464').urls.full
 
+cafe_photo_1 = Unsplash::Photo.find('e-9eDRBH4S4').urls.regular
+cafe_photo_2 = Unsplash::Photo.find('W-8VcLj2mWU').urls.regular
+cafe_photo_3 = Unsplash::Photo.find('C3HUUp5l4bM').urls.regular
 
-intro_photo = Unsplash::Photo.find('XtUd5SiX464')
+coasters_photo = Unsplash::Photo.find('7-UIl7AGWqA').urls.small
+pourover_kettle_set_photo = Unsplash::Photo.find('RkCvkHgfiqc').urls.small
+primo_passo_beans_photo = Unsplash::Photo.find('KRedbshBxEk').urls.small
 
-cafe_photo_1 = Unsplash::Photo.find('e-9eDRBH4S4')
-cafe_photo_2 = Unsplash::Photo.find('W-8VcLj2mWU')
-cafe_photo_3 = Unsplash::Photo.find('C3HUUp5l4bM')
+coffee_mug_photo_1 = Unsplash::Photo.find('8EIyi9Fd0vQ').urls.small
+coffee_mug_photo_2 = Unsplash::Photo.find('8jQ9zzTiyAw').urls.small
+coffee_mug_set_photo = Unsplash::Photo.find('vQQoZzKO9Ig').urls.small
 
-coasters_photo = Unsplash::Photo.find('7-UIl7AGWqA')
-pourover_kettle_set_photo = Unsplash::Photo.find('RkCvkHgfiqc')
-primo_passo_beans_photo = Unsplash::Photo.find('KRedbshBxEk')
+oat_milk_photo = Unsplash::Photo.find('98NbmEvXDZI').urls.small
+oat_milk_set_photo = Unsplash::Photo.find('zVx3d0e30mg').urls.small
 
-coffee_mug_photo_1 = Unsplash::Photo.find('8EIyi9Fd0vQ')
-coffee_mug_photo_2 = Unsplash::Photo.find('8jQ9zzTiyAw')
-coffee_mug_set_photo = Unsplash::Photo.find('vQQoZzKO9Ig')
+tote_photo = Unsplash::Photo.find('lihCTIOP28U').urls.small
 
-oat_milk_photo = Unsplash::Photo.find('98NbmEvXDZI')
-oat_milk_set_photo = Unsplash::Photo.find('zVx3d0e30mg')
-
-tote_photo = Unsplash::Photo.find('lihCTIOP28U')
-
-durito_espresso_photo = Unsplash::Photo.find('kj9_MarJ3BA')
-yosemite_coffee_photo = Unsplash::Photo.find('aEkOGzb1HAQ')
+durito_espresso_photo = Unsplash::Photo.find('kj9_MarJ3BA').urls.small
+yosemite_coffee_photo = Unsplash::Photo.find('aEkOGzb1HAQ').urls.small
 
 puts 'hellooooo'
-
-puts photo
+puts coasters_photo
