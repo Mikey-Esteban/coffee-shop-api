95b05f511648327105f84f2ff0387dcd9a484db8
Mikey <michaelp.esteban@gmail.com>
2021-12-29 21:02:02 -0800

    added netlify site and localhost:3000

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -13,4 +13,22 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
+
+  allow do
+    origins 'http://localhost:3000'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+
+  allow do
+    origins 'coffee-shop-mikey-esteban.netlify.app'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+
+
 end
